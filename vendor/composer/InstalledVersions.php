7c3e496143281b227c04b4f27bffe4c67b98aa8f
Domenico Nusca <dom@digitaltroop.net>
2022-05-17 10:26:21 +0200

    retina review image / adjust promo image sizes

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -30,7 +30,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '793c4718758c89460e62dc7fb80365d10e867973',
+    'reference' => '402c8436451889035b4ed8dc7dde9348ebbe30e4',
     'name' => 'barn2/document-library-lite',
   ),
   'versions' => 
@@ -42,7 +42,7 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => '793c4718758c89460e62dc7fb80365d10e867973',
+      'reference' => '402c8436451889035b4ed8dc7dde9348ebbe30e4',
     ),
   ),
 );
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => '793c4718758c89460e62dc7fb80365d10e867973',
+    'reference' => '402c8436451889035b4ed8dc7dde9348ebbe30e4',
     'name' => 'barn2/document-library-lite',
   ),
   'versions' => 
@@ -18,7 +18,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '793c4718758c89460e62dc7fb80365d10e867973',
+      'reference' => '402c8436451889035b4ed8dc7dde9348ebbe30e4',
     ),
   ),
 );
