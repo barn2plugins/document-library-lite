f853f3a2a590bddcd8ca7364c3767450d7074d02
Domenico <dom@digitaltroop.net>
2022-10-17 12:31:22 +0200

    Build plugin v1.0.3

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'barn2/document-library-lite',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'ee09e79656203232c744b57faba3b9632b33d8b7',
+        'reference' => '52fe93fc7cb4e33471d8a3ef26a9d48903e89edb',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'barn2/document-library-lite' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'ee09e79656203232c744b57faba3b9632b33d8b7',
+            'reference' => '52fe93fc7cb4e33471d8a3ef26a9d48903e89edb',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
