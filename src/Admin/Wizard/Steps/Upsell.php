1b37ac5d443f596f0cdd0165ef8ee67ca847b26b
Domenico <dom@digitaltroop.net>
2022-10-17 09:42:22 +0200

    add plugin

--- src/Admin/Wizard/Steps/Upsell.php
+++ src/Admin/Wizard/Steps/Upsell.php
@@ -28,8 +28,8 @@ class Upsell extends Cross_Selling {
 			sprintf(
 				// translators: %1$s: URL to All Access Pass page %2$s: URL to the KB about the upgrading process
 				__( 'Enhance your store with these fantastic plugins from Barn2, or get them all by upgrading to an <a href="%1$s" target="_blank">All Access Pass<a/>! <a href="%2$s" target="_blank">(learn how here)</a>', 'document-library-lite' ),
-				Util::generate_utm_url( 'https://barn2.com/wordpress-plugins/bundles/', 'dlw' ),
-				Util::generate_utm_url( 'https://barn2.com/kb/how-to-upgrade-license/', 'dlw' )
+				Util::generate_utm_url( 'https://barn2.com/wordpress-plugins/bundles/', 'dlw', $this->get_plugin() ),
+				Util::generate_utm_url( 'https://barn2.com/kb/how-to-upgrade-license/', 'dlw', $this->get_plugin() )
 			)
 		);
 		$this->set_title( esc_html__( 'Extra features', 'document-library-lite' ) );
