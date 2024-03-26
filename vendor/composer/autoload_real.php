1c79a516c88f46872a014112b726a1e60e1fbcdb
Domenico <dom@digitaltroop.net>
2024-03-26 14:29:02 +0200

    update composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,30 +23,11 @@ class ComposerAutoloaderInit8aa1d3baa100ae13f7a1ded33fbd6d4b
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit8aa1d3baa100ae13f7a1ded33fbd6d4b', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit8aa1d3baa100ae13f7a1ded33fbd6d4b', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit8aa1d3baa100ae13f7a1ded33fbd6d4b::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit8aa1d3baa100ae13f7a1ded33fbd6d4b::getInitializer($loader));
 
         $loader->register(true);
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => 'barn2/document-library-lite',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
+        'reference' => '6a80ce5372e7e3f698ee860079bc1fcb755b6d2f',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'bfea5ae60be73cf30aa712154cac55de70ac9395',
-        'name' => 'barn2/document-library-lite',
         'dev' => false,
     ),
     'versions' => array(
         'barn2/document-library-lite' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
+            'reference' => '6a80ce5372e7e3f698ee860079bc1fcb755b6d2f',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'bfea5ae60be73cf30aa712154cac55de70ac9395',
             'dev_requirement' => false,
         ),
     ),
