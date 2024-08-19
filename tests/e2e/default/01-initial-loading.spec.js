50cd2f330c2ba8c068fdd8809a6cf3d57d437947
amirition <amirition@outlook.com>
2024-08-19 22:09:08 +0000

    added the setup wizard test

--- tests/e2e/default/01-initial-loading.spec.js
+++ tests/e2e/default/01-initial-loading.spec.js
@@ -46,4 +46,74 @@ test.describe('initial loading', (props) => {
 
 		expect(setupWizardIsAccessible).toBe(true);
 	});
+
+	test( 'setup wizard works fine', async ({
+		page,
+		admin,
+		pluginUtil
+	}, testInfo) => {
+		await admin.visitAdminPage( 'admin.php?page=document-library-lite-setup-wizard' );
+		await page.waitForLoadState("networkidle");
+
+		await expect(
+			page.getByText("Welcome to Document Library Lite"),
+		).toBeVisible();
+
+		await page.getByRole("button", { name: "Next" }).click();
+		await page.waitForLoadState("networkidle");
+
+		await expect(
+			page.locator(
+				".barn2-stepper__step.is-active .barn2-stepper__step-label",
+			),
+		).toContainText("Layout and Content");
+		await expect(page.locator('input[name="columns"]')).toBeVisible();
+
+		await page.getByRole("button", { name: "Next" }).click();
+		await page.waitForLoadState("networkidle");
+
+		await expect(
+			page.locator(
+				".barn2-stepper__step.is-active .barn2-stepper__step-label",
+			),
+		).toContainText("Document links");
+		await expect(page.locator('input[name="link_text"]')).toBeVisible();
+
+		await page.getByRole("button", { name: "Next" }).click();
+		await page.waitForLoadState("networkidle");
+
+		await expect(
+			page.locator(
+				".barn2-stepper__step.is-active .barn2-stepper__step-label",
+			),
+		).toContainText("Behavior");
+		await expect(page.locator('select[name="sort_by"]')).toBeVisible();
+
+		// Lst page and the settings page
+		await page.getByRole("button", { name: "Next" }).click();
+		await page.waitForLoadState("networkidle");
+		await page.waitForTimeout(1000);
+
+		if (
+			await page
+				.locator(
+					'.barn2-stepper__step.is-active .barn2-stepper__step-label:has-text("More")',
+				)
+				.isVisible()
+		) {
+			await expect(
+				page.locator(
+					".barn2-stepper__step.is-active .barn2-stepper__step-label",
+				),
+			).toContainText("More");
+			await page.locator('a:has-text( "Finish setup" )').click();
+		}
+
+		await page.locator('a:has-text( "Settings page" )').click();
+		await page.waitForLoadState("domcontentloaded");
+
+		await expect(page.locator(".barn2-promo-inner h1")).toHaveText(
+			"Document Library Lite Settings",
+		);
+	});
 });
